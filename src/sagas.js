527d9600b707a6978dbdc3d66bd0c0bc9b6c11de
klis87 <klisiczynski@gmail.com>
2017-09-14 19:53:58 +0200

    Added getRequestInstance

--- src/sagas.js
+++ src/sagas.js
@@ -1,7 +1,11 @@
-import { setContext } from 'redux-saga/effects';
+import { getContext, setContext } from 'redux-saga/effects';
 
 import { REQUEST_INSTANCE } from './constants';
 
 export function saveRequestInstance(requestInstance) {
   return setContext({ [REQUEST_INSTANCE]: requestInstance });
+}
+
+export function getRequestInstance() {
+  return getContext(REQUEST_INSTANCE);
 }
--- src/sagas.spec.js
+++ src/sagas.spec.js
@@ -1,12 +1,18 @@
-import { setContext } from 'redux-saga/effects';
+import { getContext, setContext } from 'redux-saga/effects';
 
-import { saveRequestInstance } from './sagas';
+import { getRequestInstance, saveRequestInstance } from './sagas';
 import { REQUEST_INSTANCE } from './constants';
 
 describe('sagas', () => {
   describe('saveRequestInstance', () => {
     it('returns correct effect', () => {
       assert.deepEqual(saveRequestInstance({}), setContext({ [REQUEST_INSTANCE]: {} }));
+    });
+  });
+
+  describe('getRequestInstance', () => {
+    it('returns correct effect', () => {
+      assert.deepEqual(getRequestInstance({}), getContext(REQUEST_INSTANCE));
     });
   });
 });
