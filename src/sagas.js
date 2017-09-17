5d6dcad380590d1a834b82cb1a212fe38aaeb0ca
klis87 <klisiczynski@gmail.com>
2017-09-17 17:05:10 +0200

    Added sendRequest

--- src/sagas.js
+++ src/sagas.js
@@ -1,6 +1,7 @@
-import { getContext, setContext } from 'redux-saga/effects';
+import { call, put, getContext, setContext } from 'redux-saga/effects';
 
-import { REQUEST_INSTANCE } from './constants';
+import { success, error } from './actions';
+import { REQUEST_INSTANCE, INCORRECT_PAYLOAD_ERROR } from './constants';
 
 export function saveRequestInstance(requestInstance) {
   return setContext({ [REQUEST_INSTANCE]: requestInstance });
@@ -8,4 +9,36 @@ export function saveRequestInstance(requestInstance) {
 
 export function getRequestInstance() {
   return getContext(REQUEST_INSTANCE);
+}
+
+export function* sendRequest(action) {
+  if (!action.request) {
+    throw new Error(INCORRECT_PAYLOAD_ERROR);
+  }
+
+  const requestInstance = yield call(getRequestInstance);
+  yield put(action);
+
+  try {
+    const response = yield call(requestInstance, action);
+    yield put({
+      type: success`${action.type}`,
+      payload: {
+        data: response.data,
+        meta: action,
+      },
+    });
+
+    return response;
+  } catch (e) {
+    yield put({
+      type: error`${action.type}`,
+      payload: {
+        error: e,
+        meta: action,
+      },
+    });
+
+    return { error: e };
+  }
 }
--- src/sagas.spec.js
+++ src/sagas.spec.js
@@ -1,7 +1,9 @@
-import { getContext, setContext } from 'redux-saga/effects';
+import { getContext, setContext, call, put } from 'redux-saga/effects';
+import { cloneableGenerator } from 'redux-saga/utils';
 
-import { getRequestInstance, saveRequestInstance } from './sagas';
-import { REQUEST_INSTANCE } from './constants';
+import { getRequestInstance, saveRequestInstance, sendRequest } from './sagas';
+import { success, error } from './actions';
+import { REQUEST_INSTANCE, INCORRECT_PAYLOAD_ERROR } from './constants';
 
 describe('sagas', () => {
   describe('saveRequestInstance', () => {
@@ -13,6 +15,67 @@ describe('sagas', () => {
   describe('getRequestInstance', () => {
     it('returns correct effect', () => {
       assert.deepEqual(getRequestInstance({}), getContext(REQUEST_INSTANCE));
+    });
+  });
+
+  describe('sendRequest', () => {
+    describe('with correct payload', () => {
+      const action = { type: 'FETCH', request: {} };
+      const gen = cloneableGenerator(sendRequest)(action);
+      const requestInstance = () => {};
+      const response = { data: 'some response' };
+
+      it('gets request instance', () => {
+        assert.deepEqual(gen.next().value, call(getRequestInstance));
+      });
+
+      it('dispatches request action', () => {
+        assert.deepEqual(gen.next(requestInstance).value, put(action));
+      });
+
+      it('calls requestInstance', () => {
+        assert.deepEqual(gen.next().value, call(requestInstance, action));
+      });
+
+      it('dispatches and returns request error action when there is an error', () => {
+        const errorGen = gen.clone();
+        const requestError = new Error('Something went wrong');
+        const expected = put({
+          type: error`${action.type}`,
+          payload: {
+            error: requestError,
+            meta: action,
+          },
+        });
+
+        assert.deepEqual(errorGen.throw(requestError).value, expected);
+        assert.deepEqual(errorGen.next().value, { error: requestError });
+      });
+
+      it('dispatches request success action when reponse is successful', () => {
+        const expected = put({
+          type: success`${action.type}`,
+          payload: {
+            data: response.data,
+            meta: action,
+          },
+        });
+        assert.deepEqual(gen.next(response).value, expected);
+      });
+
+      it('returns response', () => {
+        const actual = gen.next();
+        assert.deepEqual(actual.value, response);
+        assert.isTrue(actual.done);
+      });
+    });
+
+    describe('with incorrect payload', () => {
+      it('throws error when action payload is invalid', () => {
+        const invalidAction = { type: 'FETCH' };
+        const gen = sendRequest(invalidAction);
+        assert.throws(() => gen.next(), INCORRECT_PAYLOAD_ERROR);
+      });
     });
   });
 });
