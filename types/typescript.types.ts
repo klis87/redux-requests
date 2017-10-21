0f1223e2e7fcef1ecd607378d263eecb80cf6903
klis87 <klisiczynski@gmail.com>
2017-10-21 19:22:43 +0200

    Added missing types

--- types/typescript.types.ts
+++ types/typescript.types.ts
@@ -1,5 +1,46 @@
-import { success, error, abort } from './index';
+import {
+  success,
+  error,
+  abort,
+  getActionWithSuffix,
+  fetchApiDriver,
+  createRequestInstance,
+  getRequestInstance,
+  sendRequest,
+  watchRequests,
+} from './index';
 
 success('type');
 error('type');
 abort('type');
+
+const actionModifier = getActionWithSuffix('suffix');
+actionModifier('type');
+
+fetchApiDriver.getSuccessPayload({}, {});
+fetchApiDriver.getErrorPayload({});
+const requestHandlers = fetchApiDriver.getRequestHandlers({}, {});
+requestHandlers.sendRequest({});
+
+createRequestInstance({});
+
+const requestInstanceConfig = {
+  success: actionModifier,
+  error: actionModifier,
+  abort: actionModifier,
+  driver: fetchApiDriver,
+  onRequest: request => ({}),
+  onSuccess: response => ({}),
+  onError: error => ({}),
+  onAbort: () => ({}),
+}
+createRequestInstance({}, requestInstanceConfig);
+
+getRequestInstance();
+
+sendRequest({ type: 'type', request: {} });
+sendRequest({ type: 'type', payload: { request: {} } });
+sendRequest({ type: 'type', payload: { requests: [{}] } });
+sendRequest({ type: 'type', requests: [{}, {}] }, true);
+
+watchRequests();
