3cc6eed84e5f6498e26103a262785b12e653f007
klis87 <klisiczynski@gmail.com>
2017-12-06 11:46:36 +0100

    Increased timeout for typing tests

--- packages/redux-saga-requests/types/typescript.spec.js
+++ packages/redux-saga-requests/types/typescript.spec.js
@@ -1,7 +1,9 @@
 import * as tt from 'typescript-definition-tester';
 import path from 'path';
 
-describe('TypeScript definitions', () => {
+describe('TypeScript definitions', function() {
+  this.timeout(10000);
+
   it('should compile against index.d.ts', (done) => {
     tt.compile(
       [path.join(__dirname, 'typescript.types.ts')],
