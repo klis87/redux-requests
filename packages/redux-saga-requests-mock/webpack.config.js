5d747367dbb0f9c93b0500352978da189fd59d89
klis87 <klisiczynski@gmail.com>
2018-08-19 15:42:11 +0200

    Added mock driver

---
+++ packages/redux-saga-requests-mock/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  output: {
+    library: 'ReduxSagaRequestsMock',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
