34a37bdb6bad8d5d377c221e60958fe53eb82e22
klis87 <klisiczynski@gmail.com>
2019-10-20 19:08:38 +0200

    Preserve queries structure  in denormalize

--- packages/redux-saga-requests/src/normalizers/denormalize.js
+++ packages/redux-saga-requests/src/normalizers/denormalize.js
@@ -1,15 +1,24 @@
-export const denormalize = (data, normalizedData) => {
+export const denormalize = (data, normalizedData, dependencies, path = '') => {
   if (typeof data === 'string' && data.startsWith('@@')) {
-    return denormalize(normalizedData[data], normalizedData);
+    return denormalize(
+      normalizedData[data],
+      normalizedData,
+      dependencies,
+      path,
+    );
   }
 
   if (Array.isArray(data)) {
-    return data.map(v => denormalize(v, normalizedData));
+    return data.map(v => denormalize(v, normalizedData, dependencies, path));
   }
 
   if (data !== null && typeof data === 'object') {
-    return Object.entries(data).reduce((prev, [k, v]) => {
-      prev[k] = denormalize(v, normalizedData);
+    const objectEntries = dependencies[path]
+      ? Object.entries(data).filter(([k]) => dependencies[path].includes(k))
+      : Object.entries(data);
+
+    return objectEntries.reduce((prev, [k, v]) => {
+      prev[k] = denormalize(v, normalizedData, dependencies, `${path}.${k}`);
       return prev;
     }, {});
   }
--- packages/redux-saga-requests/src/normalizers/denormalize.spec.js
+++ packages/redux-saga-requests/src/normalizers/denormalize.spec.js
@@ -2,14 +2,26 @@ import { denormalize } from './denormalize';
 
 describe('denormalize', () => {
   it('should not affect objects without any reference', () => {
-    expect(denormalize({ key: 'value' }, {})).toEqual({ key: 'value' });
-    expect(denormalize('', {})).toEqual('');
-    expect(denormalize('x', {})).toEqual('x');
-    expect(denormalize(null, {})).toEqual(null);
+    expect(denormalize({ key: 'value' }, {}, {})).toEqual({ key: 'value' });
+    expect(denormalize('', {}, {})).toEqual('');
+    expect(denormalize('x', {}, {})).toEqual('x');
+    expect(denormalize(null, {}, {})).toEqual(null);
   });
 
   it('should handle reference in direct string', () => {
-    expect(denormalize('@@1', { '@@1': { id: '1', a: 'b' } })).toEqual({
+    expect(
+      denormalize(
+        '@@1',
+        {
+          '@@1': {
+            id: '1',
+            a: 'b',
+            extra: 1,
+          },
+        },
+        { '': ['id', 'a'] },
+      ),
+    ).toEqual({
       id: '1',
       a: 'b',
     });
@@ -17,7 +29,11 @@ describe('denormalize', () => {
 
   it('should handle single reference in object', () => {
     expect(
-      denormalize({ key: 'value', ref: '@@1' }, { '@@1': { id: '1', a: 'b' } }),
+      denormalize(
+        { key: 'value', ref: '@@1' },
+        { '@@1': { id: '1', a: 'b', extra: 1 } },
+        { '.ref': ['id', 'a'] },
+      ),
     ).toEqual({ key: 'value', ref: { id: '1', a: 'b' } });
   });
 
@@ -25,7 +41,11 @@ describe('denormalize', () => {
     expect(
       denormalize(
         { key: 'value', ref: '@@1', nested: { k: 'v', anotherRef: '@@2' } },
-        { '@@1': { id: '1', a: 'b' }, '@@2': { id: '2', c: 'd' } },
+        {
+          '@@1': { id: '1', a: 'b', extra: 1 },
+          '@@2': { id: '2', c: 'd', extra: 1 },
+        },
+        { '.ref': ['id', 'a'], '.nested.anotherRef': ['id', 'c'] },
       ),
     ).toEqual({
       key: 'value',
@@ -38,7 +58,11 @@ describe('denormalize', () => {
     expect(
       denormalize(
         { key: 'value', ref: '@@1' },
-        { '@@1': { id: '1', nested: '@@2' }, '@@2': { id: '2', c: 'd' } },
+        {
+          '@@1': { id: '1', nested: '@@2', extra: 1 },
+          '@@2': { id: '2', c: 'd', extra: 1 },
+        },
+        { '.ref': ['id', 'nested'], '.ref.nested': ['id', 'c'] },
       ),
     ).toEqual({
       key: 'value',
@@ -48,14 +72,45 @@ describe('denormalize', () => {
 
   it('should handle arrays', () => {
     expect(
-      denormalize([{ key: 'value', ref: '@@1' }, 'x', '@@2'], {
-        '@@1': { id: '1', nested: '@@2' },
-        '@@2': { id: '2', c: 'd' },
-      }),
+      denormalize(
+        { x: 1, flat: ['@@1', '@@2'], nested: [{ x: '@@1' }, { x: '@@2' }] },
+        {
+          '@@1': { id: '1', nested: '@@3' },
+          '@@2': { id: '2', nested: '@@4' },
+          '@@3': { id: '3', x: 1, extra: 1 },
+          '@@4': { id: '4', x: 2, extra: 2 },
+        },
+        {
+          '.flat': ['id'],
+          '.nested.x': ['id', 'nested'],
+          '.nested.x.nested': ['id', 'x'],
+        },
+      ),
+    ).toEqual({
+      x: 1,
+      flat: [{ id: '1' }, { id: '2' }],
+      nested: [
+        { x: { id: '1', nested: { id: '3', x: 1 } } },
+        { x: { id: '2', nested: { id: '4', x: 2 } } },
+      ],
+    });
+  });
+
+  it('should handle one to one relationships', () => {
+    expect(
+      denormalize(
+        ['@@1', '@@2', '@@3'],
+        {
+          '@@1': { id: '1', bestFriend: '@@2', extra: 1 },
+          '@@2': { id: '2', bestFriend: '@@1', extra: 1 },
+          '@@3': { id: '3', bestFriend: '@@3', extra: 1 },
+        },
+        { '': ['id', 'bestFriend'], '.bestFriend': ['id'] },
+      ),
     ).toEqual([
-      { key: 'value', ref: { id: '1', nested: { id: '2', c: 'd' } } },
-      'x',
-      { id: '2', c: 'd' },
+      { id: '1', bestFriend: { id: '2' } },
+      { id: '2', bestFriend: { id: '1' } },
+      { id: '3', bestFriend: { id: '3' } },
     ]);
   });
 });
