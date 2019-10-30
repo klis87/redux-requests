d780aee9f6e497793c483f4029d022f045d4ba8b
klis87 <klisiczynski@gmail.com>
2019-10-30 18:27:25 +0100

    Refactored getQuery and getMutation as optimized selectors instead of selector creators

--- packages/redux-saga-requests/src/selectors.js
+++ packages/redux-saga-requests/src/selectors.js
@@ -1,9 +1,62 @@
-import { createSelector } from 'reselect';
+import {
+  createSelectorCreator,
+  createSelector,
+  defaultMemoize,
+} from 'reselect';
+
+import { denormalize, getDependentKeys } from './normalizers';
+
+const isEqual = (currentVal, previousVal) => {
+  if (
+    currentVal &&
+    previousVal &&
+    typeof currentVal === 'object' &&
+    'normalized' in currentVal &&
+    'normalizedData' in currentVal
+  ) {
+    if (
+      !currentVal.normalized ||
+      currentVal.normalizedData === previousVal.normalizedData
+    ) {
+      return true;
+    }
+    const currentDependencies = getDependentKeys(
+      currentVal.queryState.data,
+      currentVal.normalizedData,
+      currentVal.queryState.usedKeys,
+    );
+    const previousDependencies = getDependentKeys(
+      previousVal.queryState.data,
+      previousVal.normalizedData,
+      previousVal.queryState.usedKeys,
+    );
+
+    if (currentDependencies.size !== previousDependencies.size) {
+      return false;
+    }
+
+    for (const k of currentDependencies) {
+      if (
+        !previousDependencies.has(k) ||
+        currentVal.normalizedData[k] !== previousVal.normalizedData[k]
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return currentVal === previousVal;
+};
+
+const createCustomSelector = createSelectorCreator(defaultMemoize, isEqual);
 
 const defaultQueryState = {
   data: null,
   pending: 0,
   error: null,
+  normalized: false,
 };
 
 const getData = (data, multiple, defaultData) => {
@@ -22,35 +75,59 @@ const getData = (data, multiple, defaultData) => {
   return data;
 };
 
-export const getQuery = ({ type, defaultData, multiple = false }) =>
-  createSelector(
-    state => state.network.queries[type] || defaultQueryState,
-    queryState => ({
-      data: getData(queryState.data, multiple, defaultData),
+const getQueryState = (state, type) =>
+  state.network.queries[type] || defaultQueryState;
+
+const createQuerySelector = type =>
+  createCustomSelector(
+    state => getQueryState(state, type),
+    state => ({
+      queryState: getQueryState(state, type),
+      normalized: getQueryState(state, type).normalized,
+      normalizedData: state.network.normalizedData,
+    }),
+    (state, defaultData) => defaultData,
+    (state, defaultData, multiple) => multiple,
+    (queryState, { normalized, normalizedData }, defaultData, multiple) => ({
+      data: normalized
+        ? denormalize(
+            getData(queryState.data, multiple, defaultData),
+            normalizedData,
+            queryState.usedKeys,
+          )
+        : getData(queryState.data, multiple, defaultData),
       loading: queryState.pending > 0,
       error: queryState.error,
     }),
   );
+
+const querySelectors = {};
+
+export const getQuery = (state, { type, defaultData, multiple = false }) => {
+  if (!querySelectors[type]) {
+    querySelectors[type] = createQuerySelector(type);
+  }
+
+  return querySelectors[type](state, defaultData, multiple);
+};
 
 const defaultMutation = {
   loading: false,
   error: null,
 };
 
-export const getMutation = ({ type, requestKey }) =>
+const mutationSelectors = {};
+
+const createMutationSelector = type =>
   createSelector(
     state => state.network.mutations[type],
-    mutationContainer => {
-      if (
-        !mutationContainer ||
-        (requestKey && !mutationContainer[requestKey])
-      ) {
+    (state, requestKey) => requestKey,
+    (mutationState, requestKey) => {
+      if (!mutationState || (requestKey && !mutationState[requestKey])) {
         return defaultMutation;
       }
 
-      const mutation = requestKey
-        ? mutationContainer[requestKey]
-        : mutationContainer;
+      const mutation = requestKey ? mutationState[requestKey] : mutationState;
 
       return {
         loading: mutation.pending > 0,
@@ -58,3 +135,11 @@ export const getMutation = ({ type, requestKey }) =>
       };
     },
   );
+
+export const getMutation = (state, { type, requestKey }) => {
+  if (!mutationSelectors[type]) {
+    mutationSelectors[type] = createMutationSelector(type);
+  }
+
+  return mutationSelectors[type](state, requestKey);
+};
---
+++ packages/redux-saga-requests/src/selectors.spec.js
@@ -0,0 +1,204 @@
+import { getQuery } from './selectors';
+
+describe('selectors', () => {
+  describe('getQuery', () => {
+    const state = {
+      network: {
+        queries: {
+          QUERY: { pending: 1, error: null, data: 'data', normalized: false },
+          QUERY2: {
+            pending: 0,
+            error: null,
+            data: ['@@1', '@@2'],
+            normalized: true,
+            usedKeys: { '': ['id', 'x'] },
+          },
+          QUERY3: {
+            pending: 0,
+            error: null,
+            data: '@@3',
+            normalized: true,
+            usedKeys: {
+              '': ['id', 'nested'],
+              '.nested': ['id', 'list'],
+              '.nested.list': ['id'],
+            },
+          },
+        },
+        mutations: {},
+        normalizedData: {
+          '@@1': { id: 1, x: 1, y: 'a' },
+          '@@2': { id: 2, x: 2, y: 'a' },
+          '@@3': { id: 3, nested: '@@4' },
+          '@@4': { id: 4, list: ['@@5'] },
+          '@@5': { id: 5 },
+        },
+      },
+    };
+
+    it('returns fallback query state if not found', () => {
+      expect(
+        getQuery(
+          { network: { queries: {}, mutations: {} } },
+          { type: 'QUERY' },
+        ),
+      ).toEqual({
+        data: null,
+        loading: false,
+        error: null,
+      });
+    });
+
+    it('replaces data as null with [] when multiple true', () => {
+      expect(
+        getQuery(
+          { network: { queries: {}, mutations: {} } },
+          { type: 'QUERY', multiple: true },
+        ),
+      ).toEqual({
+        data: [],
+        loading: false,
+        error: null,
+      });
+    });
+
+    it('replaces data as custom object with {} when defaultData defined', () => {
+      expect(
+        getQuery(
+          { network: { queries: {}, mutations: {} } },
+          { type: 'QUERY', defaultData: {} },
+        ),
+      ).toEqual({
+        data: {},
+        loading: false,
+        error: null,
+      });
+    });
+
+    it('returns transformed query state if found', () => {
+      expect(getQuery(state, { type: 'QUERY' })).toEqual({
+        data: 'data',
+        loading: true,
+        error: null,
+      });
+    });
+
+    it('memoizes queries', () => {
+      const query = getQuery(state, { type: 'QUERY' });
+      expect(query).toBe(getQuery(state, { type: 'QUERY' }));
+
+      const query2 = getQuery({ ...state }, { type: 'QUERY2' });
+      expect(query).not.toBe(query2);
+
+      expect(query2).toBe(getQuery({ ...state }, { type: 'QUERY2' }));
+      expect(query).toBe(getQuery({ ...state }, { type: 'QUERY' }));
+
+      const queryWithMultiple = getQuery(
+        { ...state },
+        {
+          type: 'QUERY',
+          multiple: true,
+        },
+      );
+      expect(query).not.toBe(queryWithMultiple);
+      expect(queryWithMultiple).toBe(
+        getQuery({ ...state }, { type: 'QUERY', multiple: true }),
+      );
+    });
+
+    it('denormalizes data if necessary', () => {
+      expect(getQuery(state, { type: 'QUERY2' })).toEqual({
+        data: [{ id: 1, x: 1 }, { id: 2, x: 2 }],
+        loading: false,
+        error: null,
+      });
+    });
+
+    it('doesnt recompute on normalizedData update for not normalized query', () => {
+      const query = getQuery(state, { type: 'QUERY' });
+      const query2 = getQuery(
+        { ...state, network: { ...state.network, normalizedData: {} } },
+        { type: 'QUERY' },
+      );
+      expect(query).toBe(query2);
+    });
+
+    it('doesnt recompute for normalized query when normalizedData is not changed', () => {
+      const query = getQuery(state, { type: 'QUERY2' });
+      const query2 = getQuery({ ...state }, { type: 'QUERY2' });
+      const query3 = getQuery({ ...state }, { type: 'QUERY2' });
+      expect(query).toBe(query2);
+      expect(query).toBe(query3);
+    });
+
+    it('recomputes on relevant normalizedData update for normalized query', () => {
+      const query = getQuery(state, { type: 'QUERY2' });
+      const query2 = getQuery(
+        {
+          ...state,
+          network: {
+            ...state.network,
+            normalizedData: {
+              ...state.network.normalizedData,
+              '@@2': { id: 2, x: 3, y: 'a' },
+            },
+          },
+        },
+        { type: 'QUERY2' },
+      );
+      expect(query).toEqual({
+        data: [{ id: 1, x: 1 }, { id: 2, x: 2 }],
+        loading: false,
+        error: null,
+      });
+      expect(query2).toEqual({
+        data: [{ id: 1, x: 1 }, { id: 2, x: 3 }],
+        loading: false,
+        error: null,
+      });
+    });
+
+    it('recomputes on nested dependency update for normalized query', () => {
+      const query = getQuery(state, { type: 'QUERY3' });
+      const query2 = getQuery(
+        {
+          ...state,
+          network: {
+            ...state.network,
+            normalizedData: {
+              ...state.network.normalizedData,
+              '@@4': { id: 4, list: ['@@5', '@@6'] },
+              '@@5': { id: 5 },
+              '@@6': { id: 6 },
+            },
+          },
+        },
+        { type: 'QUERY3' },
+      );
+      expect(query).not.toEqual(query2);
+    });
+
+    it('doesnt recompute on irrelevant normalizedData update for normalized query', () => {
+      const query = getQuery(state, { type: 'QUERY2' });
+      const query2 = getQuery(
+        {
+          ...state,
+          network: {
+            ...state.network,
+            normalizedData: {
+              ...state.network.normalizedData,
+              '@@3': { id: 3, x: 4, y: 'a' },
+            },
+          },
+        },
+        { type: 'QUERY2' },
+      );
+      expect(query).toEqual({
+        data: [{ id: 1, x: 1 }, { id: 2, x: 2 }],
+        loading: false,
+        error: null,
+      });
+      expect(query2).toBe(query);
+    });
+  });
+});
