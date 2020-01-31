9e88967e2eb96442cb74c7d2c1609f5fdd6ee7d2
Franck Hourdin <frkhourdin@gmail.com>
2020-01-31 09:06:26 +0100

    Allow dynamic queries

--- packages/redux-saga-requests-graphql/types/index.d.ts
+++ packages/redux-saga-requests-graphql/types/index.d.ts
@@ -6,4 +6,7 @@ interface GraphqlDriverConfig {
 
 export const createDriver: (config: GraphqlDriverConfig) => Driver;
 
-export const gql: (query: string | TemplateStringsArray) => string;
+export const gql: (
+  query: TemplateStringsArray,
+  ...args: (string | number)[]
+) => string;
