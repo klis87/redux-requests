e1746d8727aeee631468aba45449df6e804aee5a
Dragoș Străinu <str.dr4605@gmail.com>
2020-03-25 21:30:07 +0200

    feat(promise-driver): clean up package
    #244

--- packages/redux-saga-requests-promise-driver/types/index.d.ts
+++ packages/redux-saga-requests-promise-driver/types/index.d.ts
@@ -1,1 +1,7 @@
-export const createDriver: () => any;
+import { Driver } from 'redux-saga-requests';
+
+export const createDriver: ({
+  AbortController,
+}: {
+  AbortController?: AbortController;
+}) => Driver;
