8155757e2e12de877d67c5aee09ceeb6ac2f2796
Dragoș Străinu <str.dr4605@gmail.com>
2020-03-28 00:11:36 +0200

    docs: add usage section

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -3,6 +3,7 @@ module.exports = {
     'Getting started': [
       'getting-started/motivation',
       'getting-started/installation',
+      'getting-started/usage',
     ],
     'Guide (remove before release)': ['doc1', 'doc2', 'doc3', 'mdx'],
   },
