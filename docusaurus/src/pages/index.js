acbd5e5b8d7b0949fbc984b652291007a44c9161
Dragoș Străinu <str.dr4605@gmail.com>
2020-03-28 00:05:06 +0200

    docs: add installation

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -72,7 +72,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/motivation')}
+              to={useBaseUrl('docs/getting-started/motivation')}
             >
               Get Started
             </Link>
