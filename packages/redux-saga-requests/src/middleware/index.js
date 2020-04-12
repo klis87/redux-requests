1e41e9e60980ba3983aa18f3ac77c1a3b6d9f348
klis87 <klisiczynski@gmail.com>
2020-04-12 17:23:03 +0200

    Added most tests to sendRequestsMiddleware

--- packages/redux-saga-requests/src/middleware/index.js
+++ packages/redux-saga-requests/src/middleware/index.js
@@ -2,4 +2,4 @@ export { default as createRequestsCacheMiddleware } from './create-requests-cach
 export { default as createRequestsPromiseMiddleware } from './create-requests-promise-middleware';
 export { default as createClientSsrMiddleware } from './create-client-ssr-middleware';
 export { default as createServerSsrMiddleware } from './create-server-ssr-middleware';
-export { default as createSendRequestMiddleware } from './create-send-requests-middleware';
+export { default as createSendRequestsMiddleware } from './create-send-requests-middleware';
