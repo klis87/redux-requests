86d5ab15dc9ae400031a46d3375c2f5abee830d6
klis87 <klisiczynski@gmail.com>
2020-05-19 18:43:46 +0200

    Removed regenerator runtime from all packages, as well as runtime helpers from some

--- packages/redux-requests-mock/src/mock-driver.js
+++ packages/redux-requests-mock/src/mock-driver.js
@@ -1,9 +1,10 @@
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-export const createDriver = (mockInstance, { timeout = 0 } = {}) => async (
+export const createDriver = (mockInstance, { timeout = 0 } = {}) => (
   requestConfig,
   requestAction,
 ) => {
-  await sleep(timeout);
-  return mockInstance[requestAction.type](requestConfig, requestAction);
+  return sleep(timeout).then(() =>
+    mockInstance[requestAction.type](requestConfig, requestAction),
+  );
 };
