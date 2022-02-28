5188ae4204e57befc33de1a737f48f3af7eb7d20
Chilouh Faouzi <61601372+chilouhfaouzi@users.noreply.github.com>
2022-02-28 11:12:41 +0100

    Add documentation to the basic exemple (store/index.js)
    Add documentation to store/index.js

--- examples/basic/src/store/index.js
+++ examples/basic/src/store/index.js
@@ -1,29 +1,33 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import axios from 'axios';
-import { handleRequests } from '@redux-requests/core';
-import { createDriver } from '@redux-requests/axios';
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import axios from "axios";
+import { handleRequests } from "@redux-requests/core";
+import { createDriver } from "@redux-requests/axios";
 
+// configureStore is used to create the store and configure it with the reducers and middleware
 export const configureStore = () => {
   const { requestsReducer, requestsMiddleware } = handleRequests({
     driver: createDriver(
       axios.create({
-        baseURL: 'https://jsonplaceholder.typicode.com',
-      }),
+        baseURL: "https://jsonplaceholder.typicode.com",
+      })
     ),
   });
 
+  // combineReducers is used to combine multiple reducers into one reducer
   const reducers = combineReducers({
     requests: requestsReducer,
   });
 
+  // compose is used to apply multiple middlewares to the store
   const composeEnhancers =
-    (typeof window !== 'undefined' &&
+    (typeof window !== "undefined" &&
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
     compose;
 
+  // createStore is used to create the store and apply middleware to it to handle the requests
   const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware(...requestsMiddleware)),
+    composeEnhancers(applyMiddleware(...requestsMiddleware))
   );
 
   return store;
